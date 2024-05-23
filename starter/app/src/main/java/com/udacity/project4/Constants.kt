eb26fad2c03735fe0bd31dfa4efb7b4ad855d43a
hieppham1999 <hieppham1999@gmail.com>
2024-05-24 00:36:46 +0700

    add android tests

--- starter/app/src/main/java/com/udacity/project4/Constants.kt
+++ starter/app/src/main/java/com/udacity/project4/Constants.kt
@@ -2,4 +2,4 @@ package com.udacity.project4
 
 const val MESSAGE_FETCHING_REMINDER_LIST_FAILED = "Fetching reminder list failed"
 const val MESSAGE_FETCHING_REMINDER_FAILED = "Fetching reminder failed"
-const val MESSAGE_REMINDER_NOT_FOUND = "Not found!"
+const val MESSAGE_REMINDER_NOT_FOUND = "Reminder not found!"
